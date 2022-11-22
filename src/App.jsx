00241253d965fa58caab6b9d772cb3377101d683
Pat Cornejo <pat.cornejo.a@gmail.com>
2022-11-21 22:57:21 -0300

    Clase Inicial React

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+/*export default class App extends React.Component {
+    render() {
+        return <div></div>
+    }
+}
+
+export default function App () {
+
+}*/
+import './styles.css';
+
+const App = () => {
+    return (
+        <>
+            <div className="app-header">
+                <h1> Bienvenido a Curso React</h1>
+                <h3>Este es un subtitulo para el curso de React Clase 1.</h3>
+            </div>
+            <div className="app">
+                <h3>Genial bienvenida al curso de React</h3>
+                <p>Se viene un desafío para ustedes de crear componentes</p>
+            </div>
+        </>
+    )
+}
+
+export default App
